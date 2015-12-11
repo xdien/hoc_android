dead4a94e119c07eb389f21e4e19543f88d488ec
F15079 <F15079@cusc_lab.cusc.ctu.edu.vn>
2015-12-11 18:29:02 +0700

    Them dang ky su kien button mua va xay dung alertdialog

--- gridSimple/src/com/example/gridsimple/MainActivity.java
+++ gridSimple/src/com/example/gridsimple/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.gridsimple;
 
+import java.io.Console;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 import android.app.Activity;
 import android.app.ApplicationErrorReport.AnrInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	String[] ds;
@@ -25,8 +32,18 @@ public class MainActivity extends Activity {
 		dsDoChoi.add(new DoChoi(R.drawable.toy1, "Luu dan", 109000));
 		dsDoChoi.add(new DoChoi(R.drawable.toy1, "Trai chau", 10000));
 		dsDoChoi.add(new DoChoi(R.drawable.toy3, "nguoi may", 90000));
-		MyGridCustom adtDochoi = new MyGridCustom(dsDoChoi, MainActivity.this);
+		final MyGridCustom adtDochoi = new MyGridCustom(dsDoChoi, MainActivity.this);
 		gridview = (GridView) findViewById(R.id.gridView1);
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				Log.w("Click dc nek", "ok!");
+				Toast.makeText(MainActivity.this,((DoChoi)adtDochoi.getItem(position)).ten,Toast.LENGTH_SHORT).show();
+				
+			}
+		});
 		gridview.setAdapter(adtDochoi);
 	}
 }
--- gridSimple/src/com/example/gridsimple/MyGridCustom.java
+++ gridSimple/src/com/example/gridsimple/MyGridCustom.java
@@ -3,12 +3,17 @@ package com.example.gridsimple;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -41,7 +46,7 @@ public class MyGridCustom extends BaseAdapter {
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
+	public View getView(final int position, View convertView, ViewGroup parent) {
 		ItemDoChoi motDoChoi;
 		if(convertView == null){
 			motDoChoi = new ItemDoChoi();
@@ -50,6 +55,7 @@ public class MyGridCustom extends BaseAdapter {
 			motDoChoi.hinh = (ImageView) convertView.findViewById(R.id.imHinh);
 			motDoChoi.ten = (TextView) convertView.findViewById(R.id.tvTen);
 			motDoChoi.gia = (TextView) convertView.findViewById(R.id.tvGia);
+			motDoChoi.btnMua = (Button) convertView.findViewById(R.id.btnMua);
 			convertView.setTag(motDoChoi);
 		}else {
 			motDoChoi = (ItemDoChoi)convertView.getTag();
@@ -57,11 +63,35 @@ public class MyGridCustom extends BaseAdapter {
 		motDoChoi.hinh.setImageResource(dsDochoi.get(position).hinh);
 		motDoChoi.ten.setText(dsDochoi.get(position).ten);
 		motDoChoi.gia.setText(dsDochoi.get(position).gia+"");
+		motDoChoi.btnMua.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				hienThiDoVat(dsDochoi.get(position));
+			}
+		});
 		return convertView;
+	}
+	private void hienThiDoVat(DoChoi doChoi){
+		AlertDialog.Builder dialog = new Builder(context);
+		dialog.setTitle("Mon hang");
+		dialog.setIcon(context.getResources().getDrawable(doChoi.hinh));
+		dialog.setMessage("Ten: " + doChoi.ten+ "\n" + "Gia: "+doChoi.gia);
+		dialog.setPositiveButton("dong", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+				
+			}
+		});
+		dialog.show();
 	}
 	class ItemDoChoi{
 		ImageView hinh;
 		TextView ten;
 		TextView gia;
+		Button btnMua;
 	}
 }
