a0fac807fb1da9c73090e4de8581f7a805a4fdf2
F15079 <F15079@cusc_lab.cusc.ctu.edu.vn>
2016-01-08 18:53:52 +0700

    Them in ten goi img

--- baiTestTraiCay/src/com/example/baitesttraicay/ThemTraiCayActivity.java
+++ baiTestTraiCay/src/com/example/baitesttraicay/ThemTraiCayActivity.java
@@ -1,23 +1,39 @@
 package com.example.baitesttraicay;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class ThemTraiCayActivity extends Activity {
 	EditText etName, etPath;
 	Button btnAdd, btnHinh;
+	TraiCayDbHelper traiCayHelper;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_them_trai_cay);
+		traiCayHelper = new TraiCayDbHelper(ThemTraiCayActivity.this, "traicay.sql");
 		etName = (EditText) findViewById(R.id.etName);
 		etPath = (EditText) findViewById(R.id.etPath);
 		btnAdd = (Button) findViewById(R.id.btnAdd);
 		btnHinh = (Button) findViewById(R.id.btnSelectImg);
+		btnHinh.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setAction(Intent.ACTION_VIEW);
+				intent.setDataAndType(Uri.parse("file://" + "/sdcard/test.jpg"), "image/*");
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
